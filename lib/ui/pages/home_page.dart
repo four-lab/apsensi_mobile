ba080ae23b4d9a3e11b85c177e7636c92e0e462c
Hafidzul Ahkam Dwika Maulana <ghafid999@gmail.com>
2024-04-02 21:41:49 +0700

    add navigation bar but not fix

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,29 +1,29 @@
-import 'package:apsensi_mobile/shared/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:apsensi_mobile/shared/theme.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: primary,
+      backgroundColor: whiteColor,
       body: Container(
-        decoration: BoxDecoration(
-          gradient: background,
-        ),
+        // decoration: BoxDecoration(
+        //   gradient: background,
+        // ),
         child: ListView(
           padding: const EdgeInsets.symmetric(horizontal: 25),
           children: [
             Container(
               margin: const EdgeInsets.only(top: 100, bottom: 10),
-              child: const Row(
+              child: Row(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
                   Flexible(
                     flex: 1,
                     child: CircleAvatar(
-                      radius: 50, // Adjust the size as needed
+                      radius: 50,
                       backgroundImage: AssetImage('assets/home/profil_picture.png'),
                     ),
                   ),
@@ -38,6 +38,7 @@ class HomePage extends StatelessWidget {
                           style: TextStyle(
                             fontSize: 20,
                             fontWeight: FontWeight.bold,
+                            color: buttonDisableColor, // seharusnya whiteColor, itu hanya untuk lihat hasilnya
                           ),
                         ),
                         SizedBox(height: 5),
@@ -45,11 +46,98 @@ class HomePage extends StatelessWidget {
                           "Ahmad Irsyadul Ibad",
                           style: TextStyle(
                             fontSize: 16,
+                            color: buttonDisableColor, // seharusnya whiteColor, itu hanya untuk lihat hasilnya
                           ),
                         ),
                       ],
                     ),
                   ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.home),
+        backgroundColor: buttonActiveColor,
+        foregroundColor: whiteColor,
+        elevation: 0,
+        shape: CircleBorder(),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        notchMargin: 5.0,
+        shape: CircularNotchedRectangle(),
+        color: whiteColor,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 10.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(
+                    Icons.calendar_month_outlined,
+                    color: buttonDisableColor,
+                  ),
+                  Text(
+                    "Kalender",
+                    style: greyTextStyle,
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.only(right: 20.0, top: 10.0, bottom: 10.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(
+                    Icons.book_outlined,
+                    color: buttonDisableColor,
+                  ),
+                  Text(
+                    "Jadwal",
+                    style: greyTextStyle,
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding:
+              const EdgeInsets.only(left: 20.0, top: 10.0, bottom: 10.0), // adjust padding
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(
+                    Icons.boy,
+                    color: buttonDisableColor,
+                  ),
+                  Text(
+                    "Presensi",
+                    style: greyTextStyle,
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 10.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(
+                    Icons.document_scanner_outlined,
+                    color: buttonDisableColor,
+                  ),
+                  Text(
+                    "Perizinan",
+                    style: greyTextStyle,
+                  )
                 ],
               ),
             ),
