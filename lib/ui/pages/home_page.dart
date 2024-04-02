7db46ad0178d3d57212b2fc4c0574d941a235e0c
Ahmad Muhyiddin <muhyinuris197703@gmail.com>
2024-04-02 13:27:09 +0700

    coba custom home

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -8,18 +8,53 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: primary,
-      body: ListView(
-        padding: const EdgeInsets.symmetric(horizontal: 25),
-        children: [
-        Container(
-        width: 150,
-        height: 47,
-        margin: const EdgeInsets.only(
-          top: 100,
-          bottom: 10,
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: background,
         ),
-          child: Text("Hello,\n Ahmad Irsyadul Ibad")
-        ),],
+        child: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 25),
+          children: [
+            Container(
+              margin: const EdgeInsets.only(top: 100, bottom: 10),
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Flexible(
+                    flex: 1,
+                    child: CircleAvatar(
+                      radius: 50, // Adjust the size as needed
+                      backgroundImage: AssetImage('assets/home/profil_picture.png'),
+                    ),
+                  ),
+                  SizedBox(width: 20),
+                  Flexible(
+                    flex: 2,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Hello,",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 5),
+                        Text(
+                          "Ahmad Irsyadul Ibad",
+                          style: TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
