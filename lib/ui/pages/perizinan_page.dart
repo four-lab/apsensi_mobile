fe16e65862e1fc7109e815bcdb19ab8e07fa64bb
Hafidzul Ahkam Dwika Maulana <ghafid999@gmail.com>
2024-05-02 23:34:25 +0700

    halaman perizinan, -datepicker

--- lib/ui/pages/perizinan_page.dart
+++ lib/ui/pages/perizinan_page.dart
@@ -1,22 +1,27 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:apsensi_mobile/shared/theme.dart';
+import 'package:intl/intl.dart';
 import 'home_page.dart';
 import 'jadwal_page.dart';
 import 'calendar_page.dart';
 
-class PerizinanPage extends StatelessWidget {
+class PerizinanPage extends StatefulWidget {
   const PerizinanPage({Key? key});
+
+  @override
+  State<PerizinanPage> createState() => _PerizinanPageState();
+}
+
+class _PerizinanPageState extends State<PerizinanPage> {
+  String? _selectedItem; // Letakkan variabel di sini
+  TextEditingController descriptionController = TextEditingController();
+  TextEditingController dateController= TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: transparentBackground,
-      body: Center(
-        child: Text(
-          'Ini Halaman Perizinan',
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
       bottomNavigationBar: BottomAppBar(
         color: whiteColor,
         shape: const CircularNotchedRectangle(),
@@ -39,11 +44,9 @@ class PerizinanPage extends StatelessWidget {
             fontSize: 12,
             fontWeight: medium,
           ),
-          currentIndex:
-              3, // Menandakan item terpilih, dalam hal ini indeks 3 untuk "Perizinan"
+          currentIndex: 3,
           onTap: (index) {
             if (index == 0) {
-              // Jika item "Home" diklik, kembali ke halaman Home tanpa animasi
               Navigator.pushReplacement(
                 context,
                 PageRouteBuilder(
@@ -52,7 +55,6 @@ class PerizinanPage extends StatelessWidget {
                 ),
               );
             } else if (index == 1) {
-              // Jika item "Jadwal" diklik, pindah ke halaman Jadwal tanpa animasi
               Navigator.pushReplacement(
                 context,
                 PageRouteBuilder(
@@ -61,7 +63,6 @@ class PerizinanPage extends StatelessWidget {
                 ),
               );
             } else if (index == 2) {
-              // Jika item "Kalendar" diklik, pindah ke halaman Kalendar tanpa animasi
               Navigator.pushReplacement(
                 context,
                 PageRouteBuilder(
@@ -76,8 +77,7 @@ class PerizinanPage extends StatelessWidget {
               icon: Image.asset(
                 'assets/navbar/home.png',
                 width: 24,
-                color:
-                    buttonDisableColor, // Karena bukan halaman utama, warna dinonaktifkan
+                color: buttonDisableColor,
               ),
               label: 'Home',
             ),
@@ -85,8 +85,7 @@ class PerizinanPage extends StatelessWidget {
               icon: Image.asset(
                 'assets/navbar/jadwal.png',
                 width: 24,
-                color:
-                    buttonDisableColor, // Karena bukan halaman utama, warna dinonaktifkan
+                color: buttonDisableColor,
               ),
               label: 'Jadwal',
             ),
@@ -94,8 +93,7 @@ class PerizinanPage extends StatelessWidget {
               icon: Image.asset(
                 'assets/navbar/kalender.png',
                 width: 24,
-                color:
-                    buttonDisableColor, // Karena bukan halaman utama, warna dinonaktifkan
+                color: buttonDisableColor,
               ),
               label: 'Kalendar',
             ),
@@ -103,8 +101,7 @@ class PerizinanPage extends StatelessWidget {
               icon: Image.asset(
                 'assets/navbar/perizinan.png',
                 width: 24,
-                color:
-                    buttonActiveColor, // Item "Perizinan" dipilih, sehingga warnanya aktif
+                color: buttonActiveColor,
               ),
               label: 'Perizinan',
             ),
@@ -121,6 +118,176 @@ class PerizinanPage extends StatelessWidget {
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      body: ListView(
+        padding: const EdgeInsetsDirectional.symmetric(
+          horizontal: 12,
+        ),
+        children: [
+          perizinanTittle(),
+          perizinanForm(),
+        ],
+      ),
+    );
+  }
+
+  Widget perizinanTittle() {
+    return Container(
+      margin: const EdgeInsets.only(
+        top: 40,
+      ),
+      child: Text(
+        'Pengajuan Izin',
+        style: blackTextStyle.copyWith(
+          fontSize: 18,
+          fontWeight: medium,
+        ),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+
+  Widget perizinanForm() {
+    return Container(
+      margin: const EdgeInsets.only(
+        top: 15,
+      ),
+      padding: const EdgeInsetsDirectional.all(10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 3,
+            blurRadius: 5,
+            offset: Offset(3, 3),
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Jenis Izin',
+          ),
+          SizedBox(height: 8),
+          Container(
+            decoration: BoxDecoration(
+              color: whiteColor,
+              border: Border.all(color: buttonDisableColor),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: DropdownButton<String>(
+              hint: Text('Pilih Jenis Izin'),
+              padding: EdgeInsets.only(left: 10),
+              isExpanded: true,
+              value: _selectedItem,
+              onChanged: (String? newValue) {
+                setState(() {
+                  _selectedItem = newValue!;
+                });
+              },
+              items: <String>[
+                'Dinas Keluar Kota',
+                'Sakit',
+                'Kepentingan Keluarga',
+              ].map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            ),
+          ),
+          SizedBox(height: 8),
+          Text(
+          'Deskripsi'
+          ),
+          SizedBox(height: 8),
+          TextField(
+            controller: descriptionController,
+            maxLines: 5,
+            decoration: InputDecoration(
+              hintText: "Masukkan Deskripsi",
+              border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(20))),
+              contentPadding: EdgeInsets.all(10),
+            ),
+          ),
+          SizedBox(height: 8),
+          Text(
+          'Tanggal Izin'
+          ),
+          SizedBox(height: 8),
+          
+          // tempat tanggal
+
+          SizedBox(height: 8),
+          Text(
+            'Lampiran'
+          ),
+          SizedBox(height: 8),
+          SizedBox(
+            width: double.infinity,
+            height: 300,
+            child: TextButton(
+              onPressed: () async {
+                FilePickerResult? result = await FilePicker.platform.pickFiles();
+
+                if (result != null) {
+                  PlatformFile file = result.files.first;
+
+                  print('Path: ${file.path}');
+                  print('Byte Count: ${file.bytes}');
+                  print('Name: ${file.name}');
+                  print('Extension: ${file.extension}');
+                  print('Size: ${file.size}');
+                } else {
+                  // User canceled the picker
+                }
+              },
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.black, backgroundColor: Colors.white, // Warna latar belakang menggunakan putih
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20), // Bentuk tombol
+                  side: BorderSide(color: Colors.black, width: 0.4), // Border dengan warna hitam dan lebar 0.4
+                ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                  'assets/perizinan/pdfIcon.png', // Path gambar PDF
+                  width: 48, // Lebar gambar
+                ),
+                  SizedBox(width: 8), // Jarak antara ikon dan teks
+                  Text(
+                    'Hanya file dengan format PDF', // Teks
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.grey,
+                    ), // Ukuran teks
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(height: 8),
+          SizedBox(
+            width: double.infinity, // Lebar tombol
+            height: 40, // Tinggi tombol
+            child: ElevatedButton(
+              onPressed: () {
+                // Fungsi yang akan dijalankan saat tombol ditekan
+              },
+              child: Text('Ajukan'),
+              style: ElevatedButton.styleFrom(
+                foregroundColor: Colors.black,
+                backgroundColor: buttonActiveColor,
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
