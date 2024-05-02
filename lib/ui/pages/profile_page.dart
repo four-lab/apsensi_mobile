f71059eb5d591b7a20b144b68e5851a62d94dad3
Ahmad Muhyiddin <muhyinuris197703@gmail.com>
2024-05-02 11:30:27 +0700

    add Profil Page - Home Page

---
+++ lib/ui/pages/profile_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+      ),
+    );
+  }
+}
