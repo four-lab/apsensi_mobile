309fcbb1b5cc441bdeee3dbb616453e3553544ec
Hafidzul Ahkam Dwika Maulana <ghafid999@gmail.com>
2024-05-24 15:57:10 +0700

    perbaiki halaman perizinan menjadi daftar izin dan pengajuan izin

---
+++ lib/ui/pages/pengajuan_izin.dart
@@ -0,0 +1,246 @@
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:apsensi_mobile/shared/theme.dart';
+import 'package:intl/intl.dart';
+
+class PengajuanIzinPage extends StatefulWidget {
+  const PengajuanIzinPage({Key? key}) : super(key: key);
+
+  @override
+  State<PengajuanIzinPage> createState() => _PengajuanIzinPageState();
+}
+
+class _PengajuanIzinPageState extends State<PengajuanIzinPage> {
+  String? _selectedItem;
+  TextEditingController descriptionController = TextEditingController();
+  TextEditingController dateController = TextEditingController();
+  TextEditingController daysController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: transparentBackground,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        title: Text(
+          'Pengajuan Izin',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        elevation: 0,
+        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: ListView(
+        padding: const EdgeInsetsDirectional.symmetric(
+          horizontal: 12,
+        ),
+        children: [
+          pengajuanForm(),
+        ],
+      ),
+    );
+  }
+
+  Widget pengajuanForm() {
+    return Container(
+      margin: const EdgeInsets.only(
+        top: 15,
+      ),
+      padding: const EdgeInsetsDirectional.all(10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 3,
+            blurRadius: 5,
+            offset: Offset(3, 3),
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('Jenis Izin'),
+          SizedBox(height: 8),
+          Container(
+            decoration: BoxDecoration(
+              color: whiteColor,
+              border: Border.all(color: buttonDisableColor),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: DropdownButton<String>(
+              hint: Text('Pilih Jenis Izin'),
+              padding: EdgeInsets.only(left: 10),
+              isExpanded: true,
+              value: _selectedItem,
+              onChanged: (String? newValue) {
+                setState(() {
+                  _selectedItem = newValue!;
+                });
+              },
+              items: <String>[
+                'Dinas Keluar Kota',
+                'Sakit',
+                'Kepentingan Keluarga',
+              ].map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            ),
+          ),
+          SizedBox(height: 8),
+          Text('Deskripsi'),
+          SizedBox(height: 8),
+          TextField(
+            controller: descriptionController,
+            maxLines: 5,
+            decoration: InputDecoration(
+              hintText: "Masukkan Deskripsi",
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(20))),
+              contentPadding: EdgeInsets.all(10),
+            ),
+          ),
+          SizedBox(height: 8),
+          Row(
+            children: [
+              Expanded(
+                flex: 2,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Tanggal Izin'),
+                    SizedBox(height: 8),
+                    TextField(
+                      controller: dateController,
+                      readOnly: true,
+                      decoration: InputDecoration(
+                        hintText: "Pilih Tanggal",
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(20))),
+                        contentPadding: EdgeInsets.all(10),
+                      ),
+                      onTap: () async {
+                        DateTime? pickedDate = await showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(2000),
+                          lastDate: DateTime(2101),
+                        );
+
+                        if (pickedDate != null) {
+                          String formattedDate = DateFormat('yyyy-MM-dd').format(pickedDate);
+                          setState(() {
+                            dateController.text = formattedDate;
+                          });
+                        }
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(width: 8),
+              Expanded(
+                flex: 1,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Jumlah Hari'),
+                    SizedBox(height: 8),
+                    TextField(
+                      controller: daysController,
+                      decoration: InputDecoration(
+                        hintText: "Jumlah Hari",
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(20))),
+                        contentPadding: EdgeInsets.all(10),
+                      ),
+                      keyboardType: TextInputType.number,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 8),
+          Text('Lampiran'),
+          SizedBox(height: 8),
+          SizedBox(
+            width: double.infinity,
+            height: 300,
+            child: TextButton(
+              onPressed: () async {
+                FilePickerResult? result = await FilePicker.platform.pickFiles();
+
+                if (result != null) {
+                  PlatformFile file = result.files.first;
+
+                  print('Path: ${file.path}');
+                  print('Byte Count: ${file.bytes}');
+                  print('Name: ${file.name}');
+                  print('Extension: ${file.extension}');
+                  print('Size: ${file.size}');
+                } else {
+                  // User canceled the picker
+                }
+              },
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.black,
+                backgroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  side: BorderSide(color: Colors.black, width: 0.4),
+                ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/perizinan/pdfIcon.png',
+                    width: 48,
+                  ),
+                  SizedBox(width: 8),
+                  Text(
+                    'Hanya file dengan format PDF',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(height: 8),
+          SizedBox(
+            width: double.infinity,
+            height: 40,
+            child: ElevatedButton(
+              onPressed: () {
+                // Fungsi yang akan dijalankan saat tombol ditekan
+              },
+              child: Text('Ajukan'),
+              style: ElevatedButton.styleFrom(
+                foregroundColor: Colors.black,
+                backgroundColor: buttonActiveColor,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/ui/pages/perizinan_page.dart
+++ lib/ui/pages/perizinan_page.dart
@@ -1,25 +1,21 @@
-import 'package:apsensi_mobile/ui/pages/presensi_page.dart';
-import 'package:file_picker/file_picker.dart';
+import 'dart:js';
+import 'package:apsensi_mobile/ui/pages/pengajuan_izin.dart';
 import 'package:flutter/material.dart';
 import 'package:apsensi_mobile/shared/theme.dart';
-import 'package:intl/intl.dart';
 import 'home_page.dart';
 import 'jadwal_page.dart';
 import 'calendar_page.dart';
+import 'presensi_page.dart';
+import 'pengajuan_izin.dart';
 
 class PerizinanPage extends StatefulWidget {
-  const PerizinanPage({Key? key});
+  const PerizinanPage({Key? key}) : super(key: key);
 
   @override
   State<PerizinanPage> createState() => _PerizinanPageState();
 }
 
 class _PerizinanPageState extends State<PerizinanPage> {
-  String? _selectedItem;
-  TextEditingController descriptionController = TextEditingController();
-  TextEditingController dateController = TextEditingController();
-  TextEditingController daysController = TextEditingController();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -129,224 +125,210 @@ class _PerizinanPageState extends State<PerizinanPage> {
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       body: ListView(
-        padding: const EdgeInsetsDirectional.symmetric(
-          horizontal: 12,
+        padding: const EdgeInsets.symmetric(
+          horizontal: 34,
         ),
         children: [
-          perizinanTittle(),
-          perizinanForm(),
-        ],
-      ),
-    );
-  }
-
-  Widget perizinanTittle() {
-    return Container(
-      margin: const EdgeInsets.only(
-        top: 40,
-      ),
-      child: Text(
-        'Pengajuan Izin',
-        style: blackTextStyle.copyWith(
-          fontSize: 18,
-          fontWeight: medium,
-        ),
-        textAlign: TextAlign.center,
-      ),
-    );
-  }
-
-  Widget perizinanForm() {
-    return Container(
-      margin: const EdgeInsets.only(
-        top: 15,
-      ),
-      padding: const EdgeInsetsDirectional.all(10),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(20),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.5),
-            spreadRadius: 3,
-            blurRadius: 5,
-            offset: Offset(3, 3),
-          ),
-        ],
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text('Jenis Izin'),
-          SizedBox(height: 8),
-          Container(
-            decoration: BoxDecoration(
-              color: whiteColor,
-              border: Border.all(color: buttonDisableColor),
-              borderRadius: BorderRadius.circular(20),
-            ),
-            child: DropdownButton<String>(
-              hint: Text('Pilih Jenis Izin'),
-              padding: EdgeInsets.only(left: 10),
-              isExpanded: true,
-              value: _selectedItem,
-              onChanged: (String? newValue) {
-                setState(() {
-                  _selectedItem = newValue!;
-                });
-              },
-              items: <String>[
-                'Dinas Keluar Kota',
-                'Sakit',
-                'Kepentingan Keluarga',
-              ].map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-            ),
-          ),
-          SizedBox(height: 8),
-          Text('Deskripsi'),
-          SizedBox(height: 8),
-          TextField(
-            controller: descriptionController,
-            maxLines: 5,
-            decoration: InputDecoration(
-              hintText: "Masukkan Deskripsi",
-              border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(20))),
-              contentPadding: EdgeInsets.all(10),
-            ),
-          ),
-          SizedBox(height: 8),
-          Row(
-            children: [
-              Expanded(
-                flex: 2,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('Tanggal Izin'),
-                    SizedBox(height: 8),
-                    TextField(
-                      controller: dateController,
-                      readOnly: true,
-                      decoration: InputDecoration(
-                        hintText: "Pilih Tanggal",
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.all(Radius.circular(20))),
-                        contentPadding: EdgeInsets.all(10),
-                      ),
-                      onTap: () async {
-                        DateTime? pickedDate = await showDatePicker(
-                          context: context,
-                          initialDate: DateTime.now(),
-                          firstDate: DateTime(2000),
-                          lastDate: DateTime(2101),
-                        );
-
-                        if (pickedDate != null) {
-                          String formattedDate = DateFormat('yyyy-MM-dd').format(pickedDate);
-                          setState(() {
-                            dateController.text = formattedDate;
-                          });
-                        }
-                      },
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(width: 8),
-              Expanded(
-                flex: 1,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('Jumlah Hari'),
-                    SizedBox(height: 8),
-                    TextField(
-                      controller: daysController,
-                      decoration: InputDecoration(
-                        hintText: "Jumlah Hari",
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.all(Radius.circular(20))),
-                        contentPadding: EdgeInsets.all(10),
-                      ),
-                      keyboardType: TextInputType.number,
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-          SizedBox(height: 8),
-          Text('Lampiran'),
-          SizedBox(height: 8),
-          SizedBox(
-            width: double.infinity,
-            height: 300,
-            child: TextButton(
-              onPressed: () async {
-                FilePickerResult? result = await FilePicker.platform.pickFiles();
-
-                if (result != null) {
-                  PlatformFile file = result.files.first;
-
-                  print('Path: ${file.path}');
-                  print('Byte Count: ${file.bytes}');
-                  print('Name: ${file.name}');
-                  print('Extension: ${file.extension}');
-                  print('Size: ${file.size}');
-                } else {
-                  // User canceled the picker
-                }
-              },
-              style: TextButton.styleFrom(
-                foregroundColor: Colors.black,
-                backgroundColor: Colors.white,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                  side: BorderSide(color: Colors.black, width: 0.4),
-                ),
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    'assets/perizinan/pdfIcon.png',
-                    width: 48,
-                  ),
-                  SizedBox(width: 8),
-                  Text(
-                    'Hanya file dengan format PDF',
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.grey,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          SizedBox(height: 8),
-          SizedBox(
-            width: double.infinity,
-            height: 40,
-            child: ElevatedButton(
-              onPressed: () {
-                // Fungsi yang akan dijalankan saat tombol ditekan
-              },
-              child: Text('Ajukan'),
-              style: ElevatedButton.styleFrom(
-                foregroundColor: Colors.black,
-                backgroundColor: buttonActiveColor,
-              ),
-            ),
-          ),
+          tittlePage(),
+          SizedBox(height: 10),
+          listIzin(context),
         ],
       ),
     );
   }
 }
+
+Widget tittlePage() {
+  return Container(
+    margin: const EdgeInsets.only(
+      top: 40,
+    ),
+    child: Text(
+      'Daftar Izin',
+      style: blackTextStyle.copyWith(
+        fontSize: 18,
+        fontWeight: bold,
+      ),
+      textAlign: TextAlign.center,
+    ),
+  );
+}
+
+Widget listIzin(BuildContext context) {
+  return Container(
+    padding: EdgeInsets.all(15),
+    decoration: BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(20),
+    ),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => PengajuanIzinPage()),
+            );
+          },
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+            padding: MaterialStateProperty.all<EdgeInsetsGeometry>(EdgeInsets.zero),
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+          ),
+          child: Container(
+            padding: EdgeInsets.symmetric(vertical: 10),
+            alignment: Alignment.center,
+            child: Text(
+              'Ajukan Izin Baru',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 14,
+              ),
+            ),
+          ),
+        ),
+        SizedBox(height: 10,),
+        ListView(
+          shrinkWrap: true,
+          physics: NeverScrollableScrollPhysics(),
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 10),
+                  decoration: BoxDecoration(
+                    border: Border(
+                      top: BorderSide(color: Colors.grey.withOpacity(0.5)),
+                      bottom: BorderSide(color: Colors.grey.withOpacity(0.5)),
+                    ),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Dinas Keluar Kota',
+                        style: blackTextStyle.copyWith(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Text(
+                              'Tanggal Izin: 24 Mei 2024',
+                              style: blackTextStyle.copyWith(
+                                fontSize: 13,
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                Text(
+                                  'Status Izin',
+                                  style: blackTextStyle.copyWith(
+                                    fontSize: 13,
+                                  ),
+                                ),
+                                SizedBox(height: 4), // Adding some spacing between the text
+                                Text(
+                                  'Diterima',
+                                  style: greenTextStyle.copyWith(
+                                    fontSize: 13,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 8), // Adding some spacing between the rows
+                      Text(
+                        'Lama Izin: 2 Hari',
+                        style: blackTextStyle.copyWith(
+                          fontSize: 13,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 10),
+                  decoration: BoxDecoration(
+                    border: Border(
+                      top: BorderSide(color: Colors.grey.withOpacity(0.5)),
+                      bottom: BorderSide(color: Colors.grey.withOpacity(0.5)),
+                    ),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Dinas Keluar Kota',
+                        style: blackTextStyle.copyWith(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Text(
+                              'Tanggal Izin: 24 Mei 2024',
+                              style: blackTextStyle.copyWith(
+                                fontSize: 13,
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                Text(
+                                  'Status Izin',
+                                  style: blackTextStyle.copyWith(
+                                    fontSize: 13,
+                                  ),
+                                ),
+                                SizedBox(height: 4), // Adding some spacing between the text
+                                Text(
+                                  'Diterima',
+                                  style: greenTextStyle.copyWith(
+                                    fontSize: 13,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 8), // Adding some spacing between the rows
+                      Text(
+                        'Lama Izin: 2 Hari',
+                        style: blackTextStyle.copyWith(
+                          fontSize: 13,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ],
+    ),
+  );
+}
+
+
