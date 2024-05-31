6cf8bc7bf153d1e299015abf59ea368eedbedc65
Hafidzul Ahkam Dwika Maulana <ghafid999@gmail.com>
2024-05-31 22:43:20 +0700

    menyambungkan api kalender page dan merapikan kodenya

---
+++ lib/core/models/calendar/calendar_page.dart
@@ -0,0 +1,16 @@
+class GetHolidays {
+    int id;
+    String information;
+    DateTime dateStart;
+    DateTime dateEnd;
+    String type;
+
+    GetHolidays({
+        required this.id,
+        required this.information,
+        required this.dateStart,
+        required this.dateEnd,
+        required this.type,
+    });
+
+}
