1c118cb01f547168d661dddd44584014e9b1518b
Ahmad Muhyiddin <muhyinuris197703@gmail.com>
2024-06-13 07:32:47 +0700

    add label and icon with flutter_laucher_icons,add lottie and edit splash page but not route

--- lib/ui/pages/splash_page.dart
+++ lib/ui/pages/splash_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:apsensi_mobile/shared/theme.dart';
 
 class SplashPage extends StatelessWidget {
@@ -6,12 +9,26 @@ class SplashPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Navigasi ke halaman login atau home setelah 3 detik
+    Future.delayed(Duration(seconds: 5), () async {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      if (prefs.getBool('login') == true) {
+        Get.offAllNamed('/home');
+      } else {
+        Get.offAllNamed('/login');
+      }
+    });
+
     return Scaffold(
-        backgroundColor: primary,
-        body: Center(
-            child: Container(
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [Image.asset("assets/login.png")]))));
+      backgroundColor: primary,
+      body: Center(
+        child: Lottie.asset(
+          'assets/json/splash.json',
+          width: 200, // Sesuaikan ukuran sesuai kebutuhan Anda
+          height: 200,
+          fit: BoxFit.fill,
+        ),
+      ),
+    );
   }
 }
